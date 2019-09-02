a347bfc228629d11b11dd7f731c5deec65563766
Wilson Gramer <wgramer03@gmail.com>
2019-09-01 22:17:58 -0400

    Working version

--- src/demobot_procedure.py
+++ src/demobot_procedure.py
@@ -1,126 +1,38 @@
-from botball.core.components import Motor, WheelGroup, Servo, Direction
-from botball.core.components.devices import Camera
-from botball.core.procedure import step, Procedure
+import subprocess as sp
 
-# The right motor veers off a bit, so we make the left wheel a bit slower to
-# have the robot move in a straight line
-left_motor_offset = 0.98
-right_motor_offset = 1.0
+from botball.core import step, Procedure
+from botball.core.components.devices import Motor, WheelGroup
+from botball.core.components.sensors import AnalogSensor, LightSensor, DigitalSensor
 
-# - Initialize components
+# - Components
 
-left_motor = Motor(port=1, speed=1.0)
-right_motor = Motor(port=3, speed=1.0)
+left_motor = Motor(port=0, speed=1.0)
+right_motor = Motor(port=1, speed=1.0)
+wheels = WheelGroup(left_motor, right_motor, right_offset=0.98)
 
-wheels = WheelGroup(
-    left_motor,
-    right_motor,
-    left_motor_offset,
-    right_motor_offset
-)
+light_sensor = LightSensor(port=0)
+slider = AnalogSensor(port=1)
+proximity_sensor = AnalogSensor(port=2)
 
-arm_servo = Servo(port=3, speed=0.7)
+long_button = DigitalSensor(port=0)
+top_hat = DigitalSensor(port=1)
 
-# - Game procedure methods
+# - Steps
 
-@step(name="Reset servos")
-def reset_servos():
-    arm_servo.set_position_to(0.2)
+@step(name="Demo")
+def demo():
+    while True:
+        sp.call('clear', shell=True)
 
-@step(
-    name="Get the ambulance",
-    description="Grab the ambulance on the right edge of the starting block"
-)
-def get_ambulance():
-    # From the starting block, turn and face the ambulance. Put the plow down
-    # and drive it over to the ambulance so it's in the plow.
+        print(f"Light sensor: {light_sensor.percent_value()}")
+        print(f"Slider: {slider.percent_value()}")
+        print(f"Proximity sensor: {proximity_sensor.percent_value()}")
+        print()
+        print(f"Long button: {'Pressed' if long_button.value() else 'Not pressed'}")
+        print(f"Top hat: {'Black' if top_hat.value() else 'White'} surface")
 
-    arm_servo.set_position_to(0)
-    wheels.turn_right(degrees=7.5)
-    arm_servo.set_position_to(0.32)  # touch the table
+# - Procedure
 
-    wheels.drive(mm=280)
-    wheels.turn_left(degrees=140)
-
-@step(
-    name="Drive over to buildings",
-    description="Drive over with the ambulance to the burning buildings"
-)
-def drive_to_buildings():
-    wheels.drive(mm=1350)
-
-# Store which building is burning so we know which building is safe later on.
-burning_building = None
-
-@step(
-    name="Put ambulance in safe building",
-    description="Check which building is the safe one, and dispense the ambulance in front of it."
-)
-def put_ambulance_in_safe_building():
-    # Determine which building is burning and which is safe
-
-    with Camera(tracking_color="yellow") as camera:
-        # Make sure that both a yellow AND red object is present (the marker is
-        # yellow with a red circle in the middle)
-
-        yellow_present = camera.object_is_trackable()
-
-        camera.tracking_color = "red"
-        red_present = camera.object_is_trackable()
-
-        global burning_building
-        burning_building = "first" if yellow_present and red_present else "second"
-
-    # Place the ambulance in the safe building
-
-    if burning_building == "first":
-        # The second building is the safe one; drive up to it so we can dispense
-        # The ambulance there instead
-
-        wheels.turn_right(degrees=45)
-        wheels.drive(mm=250)
-
-    # Drive right up to the building and dispense the ambulance
-
-    wheels.drive(mm=50)
-    wheels.drive(Direction.Backward, mm=100)
-
-@step(
-    name="Pick up firetruck",
-    description="Drive back to the start and obtain the firetruck"
-)
-def pick_up_firetruck():
-    if burning_building == "first":
-        # Turn around and grab the firetruck cube
-        wheels.drive(Direction.Backward, mm=530)
-        wheels.turn_right(degrees=135)
-        wheels.drive(mm=240)
-
-        # Drive up to the building
-        wheels.turn_left(degrees=245)
-        wheels.drive(380)
-    else:
-        # Turn around and grab the firetruck cube
-        wheels.turn_left(degrees=245)
-        wheels.drive(mm=380)
-
-        # Drive up to the building
-        wheels.turn_left(degrees=242)
-        wheels.drive(mm=700)
-        wheels.drive(Direction.Backward, mm=350)
-
-        # Turn around to the black line
-        wheels.turn_right(degrees=180)
-
-# - Game procedure
-
-procedure = Procedure(name="Demobot Temp", steps=[
-    reset_servos, # reset everything at the start of the game
-
-    get_ambulance,
-    drive_to_buildings,
-    put_ambulance_in_safe_building,
-    pick_up_firetruck,
-
-    reset_servos, # reset everything at the end of the game, too
+procedure = Procedure(name="Demobot", steps=[
+    demo,
 ])
