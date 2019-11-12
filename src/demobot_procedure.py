e27ecd7022f3680e6876b5f6b84af4a44e845dee
Wilson Gramer <wgramer03@gmail.com>
2019-11-12 14:45:09 -0500

    Update demo code

--- src/demobot_procedure.py
+++ src/demobot_procedure.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from botball.core.procedure import step, Procedure
 from botball.core.components import Motor, WheelGroup, Direction, Servo
 
@@ -13,10 +15,23 @@ arm_servo = Servo(port=5, speed=1.0)
 
 @step(name="Move", description="Moves across the table")
 def move():
-    wheels.drive(mm=300)
-    wheels.turn_left(70)
+    wheels.turn_right(45)
+    wheels.turn_left(45)
 
-    arm_servo.set_position_to(0.5)
+    sleep(1)
+
+    wheels.turn_right(90)
+    wheels.turn_left(90)
+
+    sleep(1)
+
+    wheels.turn_right(180)
+    wheels.turn_left(180)
+
+    sleep(1)
+
+    wheels.turn_right(360)
+    wheels.turn_left(360)
 
 # - Procedure
 
