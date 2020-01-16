5a2d254da7e522976a43da445d8acf640d70df42
Wilson Gramer <wgramer03@gmail.com>
2020-01-16 07:19:00 -0500

    Add demobot test procedure

--- src/demobot_procedure.py
+++ src/demobot_procedure.py
@@ -1,22 +1,50 @@
-from botball.core import step
+from botball.core import step, Direction
 from botball.core.procedure import Procedure
 from botball.core.components import Motor, WheelGroup, Servo
 
 # - Components
 
-left_wheel = Motor(port=3, speed=1)
+left_wheel = Motor(port=1, speed=1)
 right_wheel = Motor(port=0, speed=1)
 wheels = WheelGroup(left_wheel, right_wheel)
-arm = Servo(port=0, speed=1)
+arm = Servo(port=1, speed=1)
+claw = Servo(port=2, speed=1)
+
+def raise_arm():
+    arm.set_position_to(0.3)
+
+def lower_arm():
+    arm.set_position_to(0.0)
+
+def open_claw():
+    claw.set_position_to(0.0)
+
+def close_claw():
+    claw.set_position_to(0.5)
+
 # - Steps
 
-@step(name="Move")
-def move():
-    arm.set_position_to(0.4)
-    wheels.drive(mm=180)
+@step(name="Test")
+def test():
+    open_claw()
+    raise_arm()
+
+    # Move to the block
+    wheels.drive(mm=120)
+
+    # Pick up the block
+    lower_arm()
+    close_claw()
+    raise_arm()
+
+    # Move back to the start
+    wheels.drive(Direction.Backward, mm=120)
+
+    # Drop the block
+    open_claw()
 
 # - Procedure
 
 procedure = Procedure(name="demobot", steps=[
-    move,
+    test,
 ])
