0682a8ea4c7e208707fbdd3710994ca9003f512f
Wilson Gramer <wgramer03@gmail.com>
2020-01-16 14:29:22 -0500

    Clear procedure

--- src/demobot_procedure.py
+++ src/demobot_procedure.py
@@ -1,50 +1,15 @@
-from botball.core import step, Direction
-from botball.core.procedure import Procedure
-from botball.core.components import Motor, WheelGroup, Servo
+from botball.core import step, Procedure
 
 # - Components
 
-left_wheel = Motor(port=1, speed=1)
-right_wheel = Motor(port=0, speed=1)
-wheels = WheelGroup(left_wheel, right_wheel)
-arm = Servo(port=1, speed=1)
-claw = Servo(port=2, speed=1)
-
-def raise_arm():
-    arm.set_position_to(0.3)
-
-def lower_arm():
-    arm.set_position_to(0.0)
-
-def open_claw():
-    claw.set_position_to(0.0)
-
-def close_claw():
-    claw.set_position_to(0.5)
+# add components here
 
 # - Steps
 
-@step(name="Test")
-def test():
-    open_claw()
-    raise_arm()
-
-    # Move to the block
-    wheels.drive(mm=120)
-
-    # Pick up the block
-    lower_arm()
-    close_claw()
-    raise_arm()
-
-    # Move back to the start
-    wheels.drive(Direction.Backward, mm=120)
-
-    # Drop the block
-    open_claw()
+# define steps here
 
 # - Procedure
 
 procedure = Procedure(name="demobot", steps=[
-    test,
+    # list steps here
 ])
