2b90de262c951ea86cbc6845fe00689d6cf5f1f7
Wilson Gramer <wgramer03@gmail.com>
2020-04-23 18:14:40 -0400

    Add create to game list

--- __main__.py
+++ __main__.py
@@ -1,10 +1,11 @@
 from botball import *
 
 if __name__ == '__main__':
-    from game import demobot
+    from game import demobot, create
 
     game = choose_game(RobotConfiguration.robot_name(), [
         game_from_module('demobot', demobot),
+        game_from_module('create', create),
     ])
 
     game.run()
