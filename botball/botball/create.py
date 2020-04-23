63f8f8dc987c72a960d70c579f3b285837d818e5
Wilson Gramer <wgramer03@gmail.com>
2020-04-23 18:42:55 -0400

    Add create speed adjustment
    Co-authored-by: amanbhagat41 <amanbhagat231@gmail.com>

--- botball/botball/create.py
+++ botball/botball/create.py
@@ -1,13 +1,19 @@
+import time
 from .libwallaby import *
 from .configuration import CreateConfiguration
+from .helpers import reverse
 
 
 class Create(object):
     '''
-    Allows you to control an iRobot Create connected to the robot.
+    Allows you to control an iRobot Create connected to the robot. The default
+    speed for the Create is 1.0 and must be between 0 and 1 (1 being the
+    fastest.)
     '''
 
-    def __init__(self):
+    def __init__(self, speed=1.0):
+        self.speed = speed
+
         libwallaby.create_connect()
 
     def drive(self, direction, cm):
@@ -20,8 +26,7 @@ class Create(object):
         it on a background thread. This class is not necessarily thread-safe.
         '''
 
-        # TODO: In the future we might want to support custom speeds like Motor
-        velocity = CreateConfiguration.max_velocity
+        velocity = int(self.speed * CreateConfiguration.max_velocity)
 
         if direction == reverse:
             velocity *= -1
@@ -30,7 +35,7 @@ class Create(object):
         libwallaby.create_drive_straight(velocity)
 
         # Figure out how long to sleep as the Create travels
-        block_duration = cm * CreateConfiguration.travel_time_1_cm
+        block_duration = Create._block_duration(cm, velocity)
 
         # Block until the Create has finished traveling the specified distance
         time.sleep(block_duration)
@@ -50,3 +55,7 @@ class Create(object):
         '''
 
         pass  # TODO
+
+    @staticmethod
+    def _block_duration(cm, velocity):
+        return abs(CreateConfiguration.max_velocity * CreateConfiguration.travel_time_1_cm * cm / velocity)
