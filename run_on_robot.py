48f7f98c621b23d0cf5415f323cecce3ccde47fa
Wilson Gramer <wgramer03@gmail.com>
2020-04-23 18:21:31 -0400

    Add .botball setup file

--- run_on_robot.py
+++ run_on_robot.py
@@ -61,4 +61,5 @@ clear()
 
 # Run the game on the robot
 print('\nStarting game...\n')
+ssh.run('source /home/pi/.botball')
 ssh.run(f'source {PROJECT_PATH}/bin/botball_user_program; echo "Done"', echo=True)
